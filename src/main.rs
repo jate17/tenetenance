d9aa4c14d25d27421ab76e22bd1f87403c8f2d18
jate17 <federicomanfredi.longo@gmail.com>
2025-11-05 20:02:13 +0100

    Add Store  info func

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sysinfo::{self, System};
+use sysinfo::{self, System, Disks};
 
 
 
@@ -8,11 +8,16 @@ fn main() {
     
     let mut sys = System::new_all();
 
-    let ram: Vec<u32> = ram_check(sys);
-    
-
-    println!("Memoria: {:?} bytes ", ram);
-
+    let disks = Disks::new_with_refreshed_list();
+    println!("{:?}", disks.len());
+    for disk in &disks{
+        let d = conv_bytes_storage(disk.available_space());
+        let t = conv_bytes_storage(disk.total_space());
+        let name = disk.name();
+        println!("{name:?}");
+        println!("{d:.2}");
+        println!("{t:.2}");
+    }
 
 }
 
@@ -25,9 +30,15 @@ fn main() {
 /// # Return 
 /// - Velues in Gibaytes -> u32 
 
-fn conv_bytes(bytes: u64) -> u32 {
+fn conv_bytes_ram(bytes: u64) -> u32 {
     (bytes as f64 / 1_073_741_824.0).floor() as u32
 }
+
+
+fn conv_bytes_storage(bytes: u64) -> f64 {
+    (bytes as f64 / 1_000_000_000.0)
+}
+
 
 
 
@@ -36,7 +47,8 @@ fn conv_bytes(bytes: u64) -> u32 {
 /// - 'sys': instance 'sysinfo::System'
 /// 
 /// # Return 
-/// Vector u32
+/// Four u32 inside the vector
+///
 /// - Total Memory (GB)
 /// - Used Memory (GB)
 /// - Total Swap (GB)
@@ -46,10 +58,33 @@ fn ram_check(mut sys: System) -> Vec<u32> {
     sys.refresh_all();
 
 
-    let gb_tot : u32 = conv_bytes(sys.total_memory());
-    let gb_used : u32 = conv_bytes(sys.used_memory());
-    let swap_tot : u32 = conv_bytes(sys.total_swap());
-    let swap_used : u32 = conv_bytes(sys.used_swap());
+    let gb_tot : u32 = conv_bytes_ram(sys.total_memory());
+    let gb_used : u32 = conv_bytes_ram(sys.used_memory());
+    let swap_tot : u32 = conv_bytes_ram(sys.total_swap());
+    let swap_used : u32 = conv_bytes_ram(sys.used_swap());
 
     vec![gb_tot, gb_used, swap_tot, swap_used]
 }   
+
+/// Check Storage info
+/// 
+/// # Parms
+/// None 
+/// 
+/// # Return 
+/// 
+/// Tuple inside the vector, tuple struct is String, f64, f64
+/// The two f64 is in GB
+
+fn storage_check() -> Vec<(String, f64, f64)>{
+    let disks = Disks::new_with_refreshed_list();
+    let mut output: Vec<(String, f64,f64)> = Vec::new();
+    for disk in &disks{
+        let available_space = conv_bytes_storage(disk.available_space());
+        let total_sapce = conv_bytes_storage(disk.total_space());
+        let name = disk.name().to_string_lossy().to_string();
+        output.push((name, total_sapce, available_space));
+    }
+
+    output
+}
